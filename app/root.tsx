08166582446ea6cf595dfa53870e546b91d91182
junseinagao <junsei.nagao@gmail.com>
2022-07-22 16:49:14 +0900

    metaを変更しました。

--- app/root.tsx
+++ app/root.tsx
@@ -10,13 +10,13 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Nagao Software Development",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <Meta />
         <Links />
