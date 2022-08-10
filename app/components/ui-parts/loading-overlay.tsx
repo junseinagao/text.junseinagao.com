a8f4b2f9e00ccc3bf1cd16502f48358a1955f53a
junseinagao <junsei.nagao@gmail.com>
2022-08-10 12:41:12 +0900

    最初の600msはOverlayを表示するようにしました

--- app/components/ui-parts/loading-overlay.tsx
+++ app/components/ui-parts/loading-overlay.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import OutlineText from "~/assets/junseinagao-outline.svg";
 
 export default function LoadingOverlay() {
+  const [isFirstLoaded, setIsFirstLoaded] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsFirstLoaded(true);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div
       role="progressbar"
-      className="loading-overlay animate-flash pointer-events-none absolute top-0 left-0 h-full w-full bg-brand-base opacity-0 duration-500 hover:opacity-0"
+      className="loading-overlay bg-overlay-animation pointer-events-none fixed top-0 left-0 flex h-full w-full items-center justify-center opacity-0 duration-300"
+      style={
+        !isFirstLoaded
+          ? {
+              opacity: 1,
+            }
+          : {}
+      }
     >
-      <img src={OutlineText} alt="text.junseinagao.com" />
+      <img
+        src={OutlineText}
+        alt="text.junseinagao.com"
+        className="w-screen object-cover object-center"
+      />
     </div>
   );
 }
