d60fc6024c84ddc635ca7283d665a6371fae39e1
junseinagao <junsei.nagao@gmail.com>
2022-08-11 13:12:35 +0900

    ヘッダーがズレる問題をFIX

--- app/components/ui-parts/header.tsx
+++ app/components/ui-parts/header.tsx
@@ -3,17 +3,17 @@ import { Link } from "@remix-run/react";
 export default function Header() {
   return (
     <header>
-      <div className="flex max-h-28 items-center justify-center bg-brand-base-accent py-4 font-din-2014 font-bold">
+      <div className="box-border flex max-h-20 items-center justify-center bg-brand-base-accent py-4 font-din-2014 font-bold lg:max-h-24 lg:py-6">
         <Link
           to={"/"}
           prefetch="intent"
-          className="text-4xl text-brand-text underline  lg:text-7xl"
+          className="text-4xl leading-none text-brand-text underline lg:text-6xl"
         >
           text.junseinagao.com
         </Link>
       </div>
       <nav>
-        <ul className="flex list-none items-center justify-center gap-x-10 border-t border-b border-solid border-brand-sub py-4 text-2xl">
+        <ul className="flex h-20 list-none items-center justify-center gap-x-10 border-t border-b border-solid border-brand-sub py-4 text-2xl">
           <li>
             <Link
               to="/"
