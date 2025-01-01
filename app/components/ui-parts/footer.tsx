2c1a3b380addd54e05df6d1efd844b7375411315
junseinagao <junsei.nagao@gmail.com>
2025-01-01 16:33:52 +0900

    remove diff files

--- app/components/ui-parts/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="flex items-center justify-center bg-brand-base-accent py-4 text-brand-text">
-      ©2022 Junsei Nagao. All rights reserved.
-    </footer>
-  );
-}
--- app/routes/index.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Index from "./index";
-
-describe("index", () => {
-  it("Show title", () => {
-    render(<Index />);
-    expect(screen.getByText("Welcome to Remix")).toBeInTheDocument();
-  });
-});
