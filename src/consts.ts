99a3a46452089e02a591e05b2b641587f5979f2c
junseinagao <junsei.nagao@gmail.com>
2025-11-17 19:41:27 +0900

    ヘッダー、サイト名、ロゴを刷新

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,19 @@
-export const SITE_TITLE =
-  "text.junseinagao.com - 長尾ソフトウェア開発事務所 情報まとめサイト";
+export const SITE_TITLE = "Return J - 長尾ソフトウェア開発事務所";
 export const SITE_DESCRIPTION =
   "名古屋を中心に活動するWebエンジニア junseinagao のプロフィール並びに投稿記事をまとめています。";
+
+// Site information
+export const SITE_URL = "https://text.junseinagao.com";
+export const SITE_NAME = "Return J";
+
+// Author information
+export const AUTHOR_NAME = "Junsei Nagao";
+export const AUTHOR_URL = "https://text.junseinagao.com/me";
+
+// Social links
+export const SOCIAL_LINKS = {
+  twitter: "https://x.com/junpai_code",
+  zenn: "https://zenn.dev/junseinagao",
+  qiita: "https://qiita.com/junseinagao",
+  note: "https://note.com/junsei_nagao",
+} as const;
